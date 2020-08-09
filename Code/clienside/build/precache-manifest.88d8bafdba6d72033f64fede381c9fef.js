9a3f79ab55eb4660c917927506251f632e7f62ca
atul-gautam-au6 <atulspl2019@gmail.com>
2020-08-09 09:56:02 +0530

    submit

---
+++ Code/clienside/build/precache-manifest.88d8bafdba6d72033f64fede381c9fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2d766a10f9f4bc9249215e7dafcb21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b3ee1dd299670edba4c",
+    "url": "/static/css/main.3fcb8611.chunk.css"
+  },
+  {
+    "revision": "a6c59f235b468b14eddc",
+    "url": "/static/js/2.765c276b.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.765c276b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b3ee1dd299670edba4c",
+    "url": "/static/js/main.92fc467d.chunk.js"
+  },
+  {
+    "revision": "978ea55d3a595613ee95",
+    "url": "/static/js/runtime-main.91c9a919.js"
+  }
+]);
